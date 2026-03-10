07073c33e5b1a51f8a1a2ea8fe514327dac78935
Apolo Dev <266833479+apolodevfivem@users.noreply.github.com>
2026-03-10 11:47:57 -0300

    Adicionando as features de mandar o heart beat e a verificação de 10 em 10 segundos sobre se o resource está stopado

--- client.lua
+++ client.lua
@@ -1,2 +1,10 @@
 -- Apolo Dev --
 
+Citizen.CreateThread(function()
+    while true do
+        if NetworkIsPlayerActive(PlayerId()) then
+            TriggerServerEvent("anticheat_heartbeat")
+        end
+        Wait(5000)
+    end
+end)
--- server.lua
+++ server.lua
@@ -1,2 +1,24 @@
 -- Apolo Dev --
 
+local HeartBeats = {} -- aqui fica os players ativo com os heartbeats
+
+-- Aqui é o evento que vai receber os heartbeat 
+RegisterNetEvent("anticheat_heartbeat")
+AddEventHandler("anticheat_heartbeat", function()
+    local source = source
+
+    HeartBeats[source] = os.time()
+end)
+
+-- Thread principal das verificação em tempo dos heartbeat
+CreateThread(function()
+    while true do
+        for ThisSource, OldOsTime in pairs(HeartBeats) do
+            if (os.time() - OldOsTime) >= 30 then -- Maior que 30 segundos
+                DropPlayer(ThisSource, "Suspeita de desativação de script")
+            end
+        end
+
+        Wait(10000)
+    end
+end)
