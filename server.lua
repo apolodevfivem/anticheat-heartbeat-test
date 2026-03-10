4fe1568676f4b11c448db8a88b8a4164274e43d8
Apolo Dev <266833479+apolodevfivem@users.noreply.github.com>
2026-03-10 11:54:44 -0300

    Duvidas no server.lua sendo construidas

--- server.lua
+++ server.lua
@@ -22,3 +22,10 @@ CreateThread(function()
         Wait(10000)
     end
 end)
+
+-- Duvidas: "Será se stopado o anticheat de começo ele não adicione automaticamente o source??"
+-- Duvidas: "Se for adicionado um wait para quando o player conectar em menos de tal tempo ele não conectar seja dropado, porém pode ser que o resource seja stopado e fique mandando os heartbeat via trigger"
+-- Duvidas: "Se a de cima ai for real então se for um trigger de 5 em 5 segundos pode se passar por um player com o resource rodando normalmente, possível solução abaixo"
+-- Possível Solução: "Se for triggado de 5 em 5 segundos fora o script ou seja resource stopado e triggado de 5 em 5 segundos se passando por estar em heart beat ativo então coloca uma nova verificação,
+-- a nova verificação consistiria em pedir uma informação do server para o client e o client retorna uma resposta aleatória e definida pelo server pra evitar o a informação claramente exposta do server aqui
+-- exposta então, poderia ser criada uma resposta aleatória que não seja exposta justamente para evitar que o resource seja stopado e seja mais eficiente"
